169b4ab697f070e292d826c745dce3be930ab71f
Nat Weerawan <nat.wrw@gmail.com>
2012-08-04 14:30:14 +0700

    Use applescriptjs & asyncjs

--- lyric.js
+++ lyric.js
@@ -2,11 +2,14 @@
 
 var google = require('google')
   , jsdom = require('jsdom')
+  , applescript = require("applescript")
   , fs = require('fs')
   , _ = require('lodash')
   , url = require('url')
+  , async = require('async')
   , args = process.argv.slice(2)
-  , keyword = args.join(' ') + ' ' + 'lyrics'
+  , argv = args.length
+  , keyword = args.join(' ') + ' ' + 'lyrics';
 
 
 processorService = function (processorName, content, $) {
@@ -48,32 +51,73 @@ Util = function () {
     });
   }
 }
-google(keyword, function (err, next, links) {
-    var reqObj,
-        utilService;
-    if (err) {
-        console.error(err);
-    }
-    else {
-        utilService = new Util();
-        reqObj = utilService.metrolyrics(links);
-        jsdom.env({
-          html: reqObj.link,
-          scripts: [ 'http://code.jquery.com/jquery-1.7.2.min.js' ],
-          done: function(errors, window) {
-            var $ = window.$
-            ,   content
-            ,   hasLink
-            ,   processor;
 
-            processor = processorService(reqObj.processor, content, $);
-            console.log("--------------------------------------------------")
-            console.log("- " + reqObj.title + " -");
-            console.log("--------------------------------------------------\n")
-
-            processor.printLyric();
-            
+if (argv == 0) {
+  async.parallel([
+      function(callback) {
+        var song_name = 'tell application "iTunes" to name of current track as string';
+        applescript.execString(song_name, function(err, rtn) {
+          if (err) {
+            callback(err, null);
+          }
+          else {
+            callback(null, rtn);
           }
         });
-    }
-});
+      },
+      function(callback) {
+        var song_artist = 'tell application "iTunes" to artist of current track as string';
+        applescript.execString(song_artist, function(err, rtn) {
+          if (err) {
+            callback(err, null);
+          }
+          else {
+            callback(null, rtn );
+          }
+        });
+      }],
+      function(err, results){
+        var keyword = results.join (' ')
+        findLyric(keyword);
+      }
+      
+  );
+}
+else {
+  findLyric(keyword);
+}
+
+return 0;
+function findLyric (keyword) {
+    console.log ("FINDING ", keyword);
+    google(keyword, function (err, next, links) {
+        var reqObj
+          , utilService;
+
+        if (err) {
+            console.error(err);
+        }
+        else {
+            utilService = new Util();
+            reqObj = utilService.metrolyrics(links);
+            jsdom.env({
+              html: reqObj.link,
+              scripts: [ 'http://code.jquery.com/jquery-1.7.2.min.js' ],
+              done: function(errors, window) {
+                var $ = window.$
+                  , content
+                  , hasLink
+                  , processor;
+
+                processor = processorService(reqObj.processor, content, $);
+                console.log("--------------------------------------------------")
+                console.log("- " + reqObj.title + " -");
+                console.log("--------------------------------------------------\n")
+
+                processor.printLyric();
+                
+              }
+            });
+        }
+    });
+}
